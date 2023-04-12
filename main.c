dcac8eb7919601b96d45396ec73bd77e7b199a2e
euppal <113849268+ethanuppal@users.noreply.github.com>
2023-04-12 15:35:32 -0400

    add example render

--- main.c
+++ main.c
@@ -6,20 +6,52 @@
 #define WIDTH 500
 #define HEIGHT 500
 
-vec3_t vertex_list[] = {
-    vec3(-1, -1, 3),
+vec3_t vertex_list1[] = {
+    vec3(-2, -1, 3),
     vec3(0, 1, 5),
     vec3(1, -1, 5),
 };
-face3_t face_list[] = {
+face3_t face_list1[] = {
     face3(0, 1, 2)
 };
-mesh3_t mesh = mesh3(
-    .vertex_count = lengthof(vertex_list),
-    .vertex_list = vertex_list,
-    .face_count = lengthof(face_list),
-    .face_list = face_list,
+mesh3_t mesh1 = mesh3(
+    .vertex_count = lengthof(vertex_list1),
+    .vertex_list = vertex_list1,
+    .face_count = lengthof(face_list1),
+    .face_list = face_list1,
     .color = color24(255, 0, 0)
+);
+
+vec3_t vertex_list2[] = {
+    vec3(-1, 1, 3),
+    vec3(1.5, 2.5, 4),
+    vec3(2, 0.8, 3),
+};
+face3_t face_list2[] = {
+    face3(0, 1, 2)
+};
+mesh3_t mesh2 = mesh3(
+    .vertex_count = lengthof(vertex_list2),
+    .vertex_list = vertex_list2,
+    .face_count = lengthof(face_list2),
+    .face_list = face_list2,
+    .color = color24(255, 255, 0)
+);
+
+vec3_t vertex_list3[] = {
+    vec3(1, -1, 5),
+    vec3(-1, -1, 3),
+    vec3(1.5, 2.5, 4),
+};
+face3_t face_list3[] = {
+    face3(0, 1, 2)
+};
+mesh3_t mesh3_ = mesh3(
+    .vertex_count = lengthof(vertex_list3),
+    .vertex_list = vertex_list3,
+    .face_count = lengthof(face_list3),
+    .face_list = face_list3,
+    .color = color24(0, 100, 255)
 );
 
 camera3_t camera = camera3(
@@ -39,7 +71,7 @@ int main(int argc, char const* argv[]) {
 
     // Cast the rays.
     mesh3_t meshes[] = {
-        mesh
+        mesh1, mesh2, mesh3_
     };
     raycast(&frame, &camera, &space, lengthof(meshes), meshes);
 
